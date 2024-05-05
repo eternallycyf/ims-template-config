57ca47b7be8c8b44910a6cad407f131815dc4f3e
eternallycyf <969475322@qq.com>
2024-05-05 21:25:42 +0800

    ✨ feat: update config

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 export const footer = (repo: string) => [
   {
-    title: '相关依赖',
+    title: '相关仓库',
     items: [
       {
         title: 'ims-view-pc',
@@ -42,64 +42,88 @@ export const footer = (repo: string) => [
         url: 'https://ims-editor.vercel.app/',
         openExternal: true,
       },
-    ],
-  },
-  {
-    title: '帮助',
-    items: [
       {
-        title: 'GitHub',
-        url: `https://github.com/eternallycyf/${repo}`,
-        openExternal: true,
-      },
-      {
-        title: '更新日志',
-        url: `/changelog`,
-      },
-      {
-        title: '讨论',
-        url: `https://github.com/eternallycyf/${repo}/issues`,
+        title: 'ims-playground',
+        url: 'https://github.com/eternallycyf/ims-playground',
         openExternal: true,
       },
     ],
   },
   {
-    title: '模板及配置',
+    title: '模板、后台及配置',
     items: [
       {
         title: 'ims-template',
-        description: 'npm模板',
         url: 'https://github.com/eternallycyf/ims-template',
         openExternal: true,
       },
       {
         title: 'ims-monorepo-template',
-        description: 'monorepo模板',
         url: 'https://github.com/eternallycyf/ims-monorepo-template',
         openExternal: true,
       },
       {
+        title: 'ims-admin',
+        description: 'react18+vite5+unocss',
+        url: 'https://github.com/eternallycyf/ims-admin',
+        openExternal: true,
+      },
+      {
+        title: 'Antd-CustomComponent',
+        description: 'umi4+antd5',
+        url: 'https://github.com/eternallycyf/Antd-CustomComponent',
+        openExternal: true,
+      },
+      {
+        title: 'next-tamplate',
+        url: 'https://github.com/eternallycyf/next-tamplate',
+        openExternal: true,
+      },
+      {
+        title: 'vite5-react-template',
+        url: 'https://github.com/eternallycyf/vite5-react-template',
+        openExternal: true,
+      },
+      {
+        title: 'umi-h5-template',
+        url: 'https://github.com/eternallycyf/umi-h5-template',
+        openExternal: true,
+      },
+      {
+        title: 'taro-dva-template',
+        url: 'https://github.com/eternallycyf/taro-dva-template',
+        openExternal: true,
+      },
+      {
+        title: 'react-native-dva-template',
+        url: 'https://github.com/eternallycyf/react-native-dva-template',
+        openExternal: true,
+      },
+      {
         title: 'ims-template-config',
-        description: '配置',
+        description: '配置中心',
         url: 'https://github.com/eternallycyf/ims-template-config',
         openExternal: true,
       },
       {
+        title: 'vscode-settings',
+        url: 'https://github.com/eternallycyf/vscode-settings',
+        openExternal: true,
+      },
+      {
         title: 'eslint-config',
-        description: 'eslint配置',
         url: 'https://github.com/eternallycyf/eslint-config',
         openExternal: true,
       },
       {
         title: 'prettier-config',
-        description: 'prettier配置',
         url: 'https://github.com/eternallycyf/prettier-config',
         openExternal: true,
       },
     ],
   },
   {
-    title: '更多',
+    title: '源码',
     items: [
       {
         title: 'mini-react',
@@ -112,18 +136,110 @@ export const footer = (repo: string) => [
         openExternal: true,
       },
       {
-        title: 'lrxc-cli',
-        url: 'https://github.com/eternallycyf/lrxc-cli',
-        openExternal: true,
-      },
-      {
         title: 'ims-keep-alive',
         url: 'https://ims-keep-alive.vercel.app/',
         openExternal: true,
       },
       {
+        title: 'jsFunctionsRewrite',
+        url: 'https://github.com/eternallycyf/jsFunctionsRewrite',
+        openExternal: true,
+      },
+      {
+        title: 'ims-redux',
+        url: 'https://github.com/eternallycyf/my-redux-react-redux',
+        openExternal: true,
+      },
+      {
+        title: 'axios',
+        url: 'https://github.com/eternallycyf/eternallcyf-axios',
+        openExternal: true,
+      },
+      {
+        title: '更多',
+        url: 'https://www.yuque.com/eternallycyf/xm1ipq/ngrwp8ems5966mdm',
+        openExternal: true,
+      },
+    ],
+  },
+  {
+    title: '业务功能',
+    items: [
+      {
+        title: 'umi4-tab',
+        url: 'https://github.com/eternallycyf/umi4-tab',
+        openExternal: true,
+      },
+      {
+        title: '大文件断点续传',
+        url: 'https://github.com/eternallycyf/react-file-upload-continuingly',
+        openExternal: true,
+      },
+      {
+        title: 'qichacha',
+        url: 'https://github.com/eternallycyf/qichacha',
+        openExternal: true,
+      },
+      {
+        title: 'umi-gitlab-cicd',
+        url: 'https://github.com/eternallycyf/umi-gitlab-cicd',
+        openExternal: true,
+      },
+    ],
+  },
+  {
+    title: '其他工具',
+    items: [
+      {
+        title: 'lrxc-cli',
+        url: 'https://github.com/eternallycyf/lrxc-cli',
+        openExternal: true,
+      },
+      {
         title: 'ims-react-directive',
         url: 'https://github.com/eternallycyf/ims-react-directive',
+        openExternal: true,
+      },
+      {
+        title: 'React-AnglePicker',
+        url: 'https://github.com/eternallycyf/React-AnglePicker',
+        openExternal: true,
+      },
+      {
+        title: 'webpack-loader',
+        url: 'https://github.com/eternallycyf/my-webpack-loader',
+        openExternal: true,
+      },
+      {
+        title: 'webpack-plugin',
+        url: 'https://github.com/eternallycyf/my-webpack-plugin',
+        openExternal: true,
+      },
+      {
+        title: 'webpack-dll',
+        url: 'https://github.com/eternallycyf/webpack-dll-demo',
+        openExternal: true,
+      },
+    ],
+  },
+  {
+    title: 'TODO',
+    items: [
+      {
+        title: '我的笔记',
+        description: 'on 语雀',
+        url: 'https://www.yuque.com/eternallycyf',
+        openExternal: true,
+      },
+      {
+        title: 'ims-view-app',
+        url: 'https://github.com/eternallycyf/ims-view-app',
+        openExternal: true,
+      },
+      {
+        title: 'ims',
+        description: 'nestjs',
+        url: 'https://github.com/eternallycyf/ims',
         openExternal: true,
       },
     ],
